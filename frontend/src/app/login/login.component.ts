a598b68a5ada4e0606ed699b2b4ab78ea48634e4
Travis-CI <mike@chronobank.com>
2017-09-29 11:20:58 +0300

    Added http client.

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators} from "@angular/forms";
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -15,13 +17,33 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   email: FormControl;
   password: FormControl;
+  http: Http;
 
   onSubmit() {
     if (this.loginForm.valid) {
-
-
+      var url = "/login";
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+      var response = this.http.post(url, "", options)
+        .map(this.extractData)
+        .catch(this.handleErrorObservable);
       console.log("Form Submitted! email: " + this.email.value + ", pass: " + this.password.value);
     }
+  }
+
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+
+  private handleErrorObservable (error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
+  private handleErrorPromise (error: Response | any) {
+    console.error(error.message || error);
+    return Promise.reject(error.message || error);
   }
 
   ngOnInit() {
