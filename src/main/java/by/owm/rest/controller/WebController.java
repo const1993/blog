aff2bbb7fe1b4f3704dee082c756f46376da1324
Belko <siarhei.belko@instinctools.ru>
2017-12-04 15:03:19 +0300

    refactor web  controller

--- src/main/java/by/owm/rest/controller/WebController.java
+++ src/main/java/by/owm/rest/controller/WebController.java
@@ -7,34 +7,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
 
 @RestController
-@RequestMapping(value="/api")
+@RequestMapping(value = "/api")
 public class WebController {
 
-	private Map<Integer, Customer> customers = new HashMap<Integer, Customer>(){
+    private Map<Integer, Customer> customers = Stream.of(
+            new Customer(1, "Mary", "Taylor", 24),
+            new Customer(2, "Peter", "Smith", 18),
+            new Customer(3, "Lauren", "Taylor", 31)
+    ).collect(toMap(Customer::getId, identity()));
 
-		{
-	        put(1, new Customer(1, "Mary", "Taylor", 24));
-	        put(2, new Customer(2, "Peter", "Smith", 18));
-	        put(3, new Customer(3, "Lauren", "Taylor", 31));
-	    }
-	};
-	
-	@GetMapping(value="/customer",  produces=MediaType.APPLICATION_JSON_VALUE)
-	public List<Customer> getAll(){
-		List<Customer> results = customers.entrySet().stream()
-									.map(entry ->entry.getValue())
-									.collect(Collectors.toList());
-		return results;
-	}
-	
-	@GetMapping(value="/customer/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
-	public Customer getCustomer(@PathVariable int id){
-		return this.customers.get(id);
-	}
+
+    @GetMapping(value = "/customer", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Customer> getAll() {
+        return customers.entrySet().stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/customer/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Customer getCustomer(@PathVariable int id) {
+        return this.customers.get(id);
+    }
 }
