c81b9d6e67702e1b7534d4f7fc6a909f29eba961
Belko <siarhei.belko@instinctools.ru>
2017-12-08 14:05:58 +0300

    add mapper test

---
+++ src/test/java/by/owm/domain/mapper/MapperManagementTest.java
@@ -0,0 +1,53 @@
+package by.owm.domain.mapper;
+
+import by.owm.AbstractTest;
+import by.owm.domain.model.User;
+import by.owm.rest.dto.UserDto;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.assertEquals;
+
+public class MapperManagementTest extends AbstractTest {
+
+    @Autowired
+    private MapperManagement mapper;
+
+    @Test
+    public void test_map_userDto2User_expect_success() {
+
+        final UserDto dto = new UserDto(
+                "name",
+                "surname",
+                "mail",
+                "token",
+                emptyList());
+
+        final User user = mapper.map(dto, User.class);
+
+        assertEquals(dto.getName(), user.getName());
+        assertEquals(dto.getSurname(), user.getSurname());
+        assertEquals(dto.getEmail(), user.getEmail());
+        assertEquals(dto.getToken(), user.getToken());
+    }
+
+
+    @Test
+    public void test_map_user2UserDto_expect_success() {
+
+        final User user = new User(
+                "name",
+                "surname",
+                "mail",
+                "token",
+                emptyList());
+
+        final UserDto dto = mapper.map(user, UserDto.class);
+
+        assertEquals(user.getName(), dto.getName());
+        assertEquals(user.getSurname(), dto.getSurname());
+        assertEquals(user.getEmail(), dto.getEmail());
+        assertEquals(user.getToken(), dto.getToken());
+    }
+}
